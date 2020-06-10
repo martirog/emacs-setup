b4742188d11e8d97c4bc99c336d3a9c0dacd1153
Martin Rognerud <mrogneru@yahoo.no>
2020-06-10 22:15:40 +0200

    add occurens key binding insearch

--- init.el
+++ init.el
@@ -94,7 +94,11 @@
           '(lambda ()
              (switch-to-buffer-other-window "*grep*")))
 (global-set-key (quote [?\C-c ?o]) (quote find-file-at-point) )
-
+;; Activate occur easily inside isearch
+(define-key isearch-mode-map (kbd "C-o")
+  (lambda () (interactive)
+    (let ((case-fold-search isearch-case-fold-search))
+      (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; cc-mode  (tandberg c style)
