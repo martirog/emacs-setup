78fcc2c0d171f5a8a5f2005d32a37ccfdff7c68c
Martin Rognerud <mrognerud@graphcore.ai>
2020-06-25 17:32:22 +0200

    add key shourt cut for quick-calc

--- init.el
+++ init.el
@@ -99,6 +99,8 @@
   (lambda () (interactive)
     (let ((case-fold-search isearch-case-fold-search))
       (occur (if isearch-regexp isearch-string (regexp-quote isearch-string))))))
+(global-set-key (kbd "C-c +") 'quick-calc)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; cc-mode  (tandberg c style)
