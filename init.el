4188fc1cee9ffeeac2b1e547e254c3dddbdc235e
Martin Rognerud <mrogneru@yahoo.no>
2020-09-04 15:37:04 +0200

    revert previous added wrong stuff

--- init.el
+++ init.el
@@ -9,7 +9,3 @@
 (org-babel-load-file
  (expand-file-name "setup.org"
                    user-emacs-directory))
-
-(org-babel-load-file
- (expand-file-name "xps-setup.org"
-                   user-emacs-directory))
